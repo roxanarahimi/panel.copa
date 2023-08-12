af79cef195ffc026504a997cc7e002155d186226
Roxana <ms.roxanarahimi@gmail.com>
2023-08-12 13:30:38 +0330

    make thumbs

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -31,11 +31,11 @@ class ImageController extends Controller
 //        return $request;
         $image_parts = explode(";base64,", $request['code']);
         $image_base64 = base64_decode($image_parts[1]);
-        $name = str_replace('.jpg', uniqid().'.jpg',$request['name']);
+        $name = str_replace('.jpg', uniqid() . '.jpg', $request['name']);
         file_put_contents($request['path'] . $name, $image_base64);
-        $file = '/'.$request['path'] . $name;
+        $file = '/' . $request['path'] . $name;
 //        $this->resizeImage($request['path'],$name);
-        return response($file,200);
+        return response($file, 200);
 
     }
 
@@ -45,19 +45,22 @@ class ImageController extends Controller
         try {
             $image_name = $path . $name;
 
-                $image = imagecreatefrompng($image_name);
-                $imgResized = imagescale($image, 500, 500);
-                imagealphablending($imgResized, false);
-                $transparency = imagecolorallocatealpha($imgResized, 0, 0, 0, 127);
-                imagefill($imgResized, 0, 0, $transparency);
-                imagesavealpha($imgResized, true);
-                $image_name = str_replace('.png', '_thumb.png', $image_name);
-                imagepng($imgResized, $image_name);
-                $image = imagecreatefrompng($image_name);
+            $image = imagecreatefrompng($image_name);
+            $width = imagesx($image);
+            $height = imagesy($image);
+            $newWidth = 300;
+            $newHeight = $height*$newWidth/$width;
+            $imgResized = imagescale($image, $newWidth, $newHeight);
+            imagealphablending($imgResized, false);
+            $transparency = imagecolorallocatealpha($imgResized, 0, 0, 0, 127);
+            imagefill($imgResized, 0, 0, $transparency);
+            imagesavealpha($imgResized, true);
+            $image_name = str_replace('.png', '_thumb.png', $image_name);
+            imagepng($imgResized, $image_name);
+            $image = imagecreatefrompng($image_name);
 
 
-
-        }catch (\Exception $exception){
+        } catch (\Exception $exception) {
             return $exception;
         }
     }
