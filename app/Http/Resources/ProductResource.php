4871de340e05dc37ea85d769b4d5653edc5137bc
Roxana <ms.roxanarahimi@gmail.com>
2024-07-28 09:42:12 +0330

    arabic parts

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -53,7 +53,7 @@ class ProductResource extends JsonResource
                 'title_en' => $this->category->title_en,
                 'title_ar' => $this->category->title_ar,
                 'active' => $this->category->active,
-                "subCategories" => $this->subCategories,
+                "subCategories" => $this->category->subCategories,
             ],
             "related_products" => $relatedProducts,
             "color" => $this->color,
